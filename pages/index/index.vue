0592704476026281eb3b83e9c9290f44e55150c1
king <1223781603@qq.com>
2023-03-22 21:00:51 +0800

    App.vue

---
+++ pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="">
+		<swiper indicatorDots:true autoplay:true>
+			<swiper-item><img :src="src" alt=""> </swiper-item>
+			<swiper-item><img :src="src" alt=""> </swiper-item>
+			<swiper-item><img :src="src" alt=""> </swiper-item>
+			<swiper-item><img :src="src" alt=""> </swiper-item>
+
+
+		</swiper>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				src: 'https://img2.baidu.com/it/u=345670089,3951600800&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
